342e149ec2e38a8e96679cb83392b3df1b34696b
Denis Leonte <denis.leonte123@gmail.com>
2020-04-11 11:39:00 +0300

    Added SDL to the SolDir

--- 2D_Engine/main.cpp
+++ 2D_Engine/main.cpp
@@ -1,6 +1,9 @@
 #include "SDL.h"
+#undef main
 
-int main()
+using namespace std;
+
+int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
